8e4eeda0b3c3ac33999ad66440ed3ca0eb4d8804
Venugopal <keerthivenugopal.trading@gmail.com>
2026-03-07 21:24:26 +0530

    Force IPv4 for Gmail SMTP

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,15 +1,16 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+// Force IPv4
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // important
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
